c5846eb3e9d01b65756e71eaf86a7e4e21872b9a
Noam Honig <noam@fireflymigration.com>
2021-01-21 13:22:55 +0200

    init remult

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { TestBed, async } from '@angular/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'remult-angular-code-samples'`, () => {
+  it(`should have as title 'radweb-starterkit'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('remult-angular-code-samples');
+    expect(app.title).toEqual('radweb-starterkit');
   });
 
-  it('should render title', () => {
+  it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('remult-angular-code-samples app is running!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to radweb-starterkit!');
   });
 });
